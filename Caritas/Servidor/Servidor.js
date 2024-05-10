951cd2ba641d325771fbf3c1e33c68b383494f23
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2024-05-10 13:39:19 -0300

    iniciar sesion- servidor

--- Caritas/Servidor/Servidor.js
+++ Caritas/Servidor/Servidor.js
@@ -7,10 +7,25 @@ const PORT = 3000;
 const jwt = require("jsonwebtoken");
 const db = require('../database/conexion.js');
 
+const path= require("path");
 
 app.use(cors());
 app.use(bodyParser.json());
 const secretKey = "codigo secreto";
+
+
+app.use(express.static(path.join(__dirname, '../')));
+
+app.get('/registrar', (req, res)=> {
+    const filePath= path.join(__dirname, '..', 'registro.html');
+    res.sendFile(filePath);
+})
+
+
+app.get('/login', (req, res)=> {
+    const filePath = path.join(__dirname, '..', 'login.html');
+    res.sendFile(filePath);
+})
 
 app.post("/registrar", async (req, res) => {
   var { correo, password, usuario, nombre, apellido, nacimiento, dni, tlf } =
@@ -67,6 +82,43 @@ app.post("/registrar", async (req, res) => {
     res.status(500).json({ error: "Error al registrar usuario" });
   }
 });
+
+app.post('/login', async (req, res) => {
+  const { correo, password } = req.body;
+
+  // Consulta a la base de datos para obtener el usuario por correo
+  db.query('SELECT * FROM usuarios WHERE Correo = ?', [correo], async (err, results) => {
+      if (err) {
+          console.error('Error al buscar el usuario:', err);
+          return res.status(500).json({ error: 'Error interno del servidor' });
+      }
+
+      // Verificar si se encontró el usuario
+      if (results.length === 0) {
+          return res.status(401).json({ error: 'Correo electrónico o contraseña incorrectos' });
+      }
+
+      const usuario = results[0];
+
+      // Comparar la contraseña proporcionada con la almacenada en la base de datos
+      const passwordMatch = await bcrypt.compare(password, usuario.Contraseña);
+
+      if (!passwordMatch) {
+          return res.status(401).json({ error: 'Correo electrónico o contraseña incorrectos' });
+      }
+
+      const rol = usuario.rol;
+
+      // Generar un token JWT con el rol incluido y una expiración de 1 hora
+      const token = jwt.sign({ correo: usuario.Correo, rol }, secretKey, { expiresIn: '1h' });
+
+      // Responder con el token JWT, el rol del usuario y un mensaje de éxito
+      res.json({ token, rol, message: 'Inicio de sesión exitoso' });
+  });
+});
+
+
+
 app.listen(PORT, () => {
   console.log(
     `Servidor Express en ejecución en http://localhost:${PORT}/registrar`
