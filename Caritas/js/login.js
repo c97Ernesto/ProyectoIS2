7f1a2aa37f02d62cf23105bc19c84be864c5e7d0
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2024-05-10 13:47:09 -0300

    iniciar sesion

---
+++ Caritas/js/login.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+    
+        const formData = new FormData(loginForm);
+        const loginData = {
+            correo: formData.get('email'),
+            password: formData.get('password')
+        };
+    
+        try {
+            const response = await fetch('/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(loginData)
+            });
+    
+            if (!response.ok) {
+                alert('Correo electrónico o contraseña incorrectos');
+                return;
+            }
+    
+            const data = await response.json();
+            const { token, rol } = data;
+    
+            if (token) {
+                localStorage.setItem('token', token);
+    
+                // Redirigir al usuario a la página adecuada según el rol
+                if (rol === 'comun') {
+                    alert("soy un usario comun")
+                    window.location.href = '/inicio.html';
+                } else if (rol === 'voluntario') {
+                    window.location.href = '/voluntario.html';
+                } else if (rol === 'administrador') {
+                    window.location.href = '/admin.html';
+                }
+            } else {
+                alert('Error al iniciar sesión');
+            }
+        } catch (error) {
+            console.error('Error en la solicitud:', error);
+            alert('Error en la solicitud: ' + error.message);
+        }
+});
+});
